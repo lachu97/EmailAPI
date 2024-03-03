a89219b59707e853528a742f3eb14ce0290e0eeb
Lakshu Narasu <ced15i004@iiitdm.ac.in>
2024-03-03 13:51:47 +0530

    New Commit

--- index.js
+++ index.js
@@ -1,36 +1,47 @@
-const express = require('express')
-const ElasticEmail = require('@elasticemail/elasticemail-client');
-const app = express()
+const express = require("express");
+const ElasticEmail = require("@elasticemail/elasticemail-client");
+const app = express();
 const PORT = process.env.PORT || 5000;
 let defaultClient = ElasticEmail.ApiClient.instance;
-let apikey = defaultClient.authentications['apikey'];
+let apikey = defaultClient.authentications["apikey"];
 
-apikey.apiKey = "D9A6BCB0E501CEBE7B7E4EF8BDD0D8D48A606746307FDA9184D124881623F337DFBB6C15CF381FE17A171FCFBF2EDD3E"
+apikey.apiKey =
+  "D9A6BCB0E501CEBE7B7E4EF8BDD0D8D48A606746307FDA9184D124881623F337DFBB6C15CF381FE17A171FCFBF2EDD3E";
+let emailApi = new ElasticEmail.EmailsApi();
 
-app.use(express.json())
-app.get('/',(req,res) => {
-    res.send('Welcome to Email SEnd API')
-})
-app.get('/delete',(req,res) => {
-let api = new ElasticEmail.CampaignsApi()
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Welcome to Email SEnd API");
+});
 
-let name = "name_example"; // {String} Name of Campaign to delete
+app.get("/send", (req, res) => {
+  try {
+    let email = ElasticEmail.EmailMessageData.constructFromObject({
+      Recipients: [new ElasticEmail.EmailRecipient("MeowWow ")],
+      Content: {
+        Body: [
+          ElasticEmail.BodyPart.constructFromObject({
+            ContentType: "HTML",
+            Content: "My test email content ;)",
+          }),
+        ],
+        Subject: "JS EE lib test",
+        From: "betelguesebusiness@gmail.com",
+      },
+    });
 
-let callback = function(error, data, response) {
+    var callback = function (error, data, response) {
+      if (error) {
+        console.error(error);
+        res.send(response.text);
+      } else {
+        console.log("API called successfully.");
+        res.send(data);
+      }
+    };
 
-  if (error) {
+    emailApi.emailsPost(email, callback);
+  } catch (e) {}
+});
 
-    console.error(error);
-    res.send(error);
-
-  } else {
-
-    console.log('API called successfully.');
-    res.send('API called successfully.')
-
-  }
-
-};
-    api.campaignsByNameDelete(name, callback);
-})
-app.listen(PORT,()=>console.log("Serveer Runnign on PORT"))
+app.listen(PORT, () => console.log("Serveer Runnign on PORT"));
